d804ee2b8b3eaab722d61accbe4e2b2ef46e331f
wuhaifei215 <wuhaifei215@gmail.com>
2025-09-08 17:03:05 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
+//$serverMainIp = $_SERVER['SERVER_ADDR'];
+//echo "服务器的主IP地址是: " . $serverMainIp;
+//die;
+
 // 检测PHP环境
 if (version_compare(PHP_VERSION, '5.4.0', '<')) {
     die('require PHP > 5.4.0 !');
