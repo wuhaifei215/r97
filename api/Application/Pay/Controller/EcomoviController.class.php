216f4cb768d0a1c20dd25873e58781fae7af2af4
wuhaifei215 <wuhaifei215@gmail.com>
2025-09-10 17:01:15 +0800

    Update EcomoviController.class.php

--- api/Application/Pay/Controller/EcomoviController.class.php
+++ api/Application/Pay/Controller/EcomoviController.class.php
@@ -232,10 +232,8 @@ class EcomoviController extends PayController
                 CURLOPT_CUSTOMREQUEST => 'POST',
                 CURLOPT_POSTFIELDS => $json,
                 CURLOPT_HTTPHEADER => $header,
-                CURLOPT_SSLCERT => 'https://testapi.r97pay.com/cert/r97pay.com_certificate.pem',
-                CURLOPT_SSLKEY => 'https://testapi.r97pay.com/cert/r97pay.com_private.key',
-                CURLOPT_SSL_VERIFYPEER => true,
-                CURLOPT_SSL_VERIFYHOST => 2,
+                CURLOPT_SSL_VERIFYPEER => false,
+                CURLOPT_SSL_VERIFYHOST => 0,
             ));
             $response = curl_exec($curl);
             $result = [];
