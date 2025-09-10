9957212149e4ee0a7439ba278e5b26f943a2c1c3
wuhaifei215 <wuhaifei215@gmail.com>
2025-09-10 14:00:47 +0800

    Update EcomoviController.class.php

--- api/Application/Pay/Controller/EcomoviController.class.php
+++ api/Application/Pay/Controller/EcomoviController.class.php
@@ -228,10 +228,10 @@ class EcomoviController extends PayController
                 CURLOPT_MAXREDIRS => 0,
                 CURLOPT_TIMEOUT => 10,
                 CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => 'POST',
                 CURLOPT_POSTFIELDS => $json,
                 CURLOPT_HTTPHEADER => $header,
+                CURLOPT_SSL_VERIFYPEER =>0,
             ));
             $response = curl_exec($curl);
             $result = [];
