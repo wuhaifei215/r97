088a0b2bfda2efecfe19615444a0a041fecc8334
wuhaifei215 <wuhaifei215@gmail.com>
2025-09-10 16:19:19 +0800

    Update EcomoviController.class.php

--- api/Application/Pay/Controller/EcomoviController.class.php
+++ api/Application/Pay/Controller/EcomoviController.class.php
@@ -228,7 +228,7 @@ class EcomoviController extends PayController
             curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
             curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
             curl_setopt($curl, CURLOPT_POSTFIELDS, $json); // Post提交的数据包
-            curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+            curl_setopt($curl, CURLOPT_TIMEOUT, 10); // 设置超时限制防止死循环
             curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
             curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
