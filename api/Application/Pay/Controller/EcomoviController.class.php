1c8e62095dfe3edd936105f9950deb3ccaf3ec6b
wuhaifei215 <wuhaifei215@gmail.com>
2025-09-11 23:30:31 +0800

    Update EcomoviController.class.php

--- api/Application/Pay/Controller/EcomoviController.class.php
+++ api/Application/Pay/Controller/EcomoviController.class.php
@@ -240,19 +240,27 @@ class EcomoviController extends PayController
 //                CURLOPT_SSL_VERIFYPEER => false,
 //                CURLOPT_SSL_VERIFYHOST => 0,
 //            ));
-            $curl = curl_init(); // 启动一个CURL会话
-            curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); // 从证书中检查SSL加密算法是否存在
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
-            curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
-            curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $json); // Post提交的数据包
-            curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
-            curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
-            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
 
+//            $curl = curl_init(); // 启动一个CURL会话
+//            curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
+//            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+//            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); // 从证书中检查SSL加密算法是否存在
+//            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+//            curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+//            curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
+//            curl_setopt($curl, CURLOPT_POSTFIELDS, $json); // Post提交的数据包
+//            curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+//            curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+//            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+//            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $json); // Post提交的数据包
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
 
             $response = curl_exec($curl);
             $result = [];
