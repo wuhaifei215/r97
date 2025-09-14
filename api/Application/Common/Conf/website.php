ef061e648ac6dc93bf5510c0fc1d56c5d26baa0f
wuhaifei215 <wuhaifei215@gmail.com>
2025-09-14 23:20:06 +0800

    Update website.php

--- api/Application/Common/Conf/website.php
+++ api/Application/Common/Conf/website.php
@@ -1,8 +1,8 @@
 <?php
 return array(
     'WEB_TITLE' => 'R97国际支付',
-    'DOMAIN' => 'api.r97pay.com',
-    'NOTIFY_DOMAIN' => 'napi.r97pay.com',
+    'DOMAIN' => 'testapi.r97pay.com',
+    'NOTIFY_DOMAIN' => 'testapi.r97pay.com',
     'MODULE_ALLOW_LIST'   => array('Home','Pay','Cashier','Payment','Telegram','Cli'),
     'URL_MODULE_MAP'  => array(),
     'LOG_API_URL' => 'https://log.r97pay.com',
