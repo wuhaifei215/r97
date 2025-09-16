40f66acc6a3c2dd049dbcf95e2b04030399edf77
wuhaifei215 <wuhaifei215@gmail.com>
2025-09-16 18:20:11 +0800

    Update ApiController.class.php

--- open/Application/Telegram/Controller/ApiController.class.php
+++ open/Application/Telegram/Controller/ApiController.class.php
@@ -775,7 +775,7 @@ class ApiController extends Controller
     }
 
     //代收处理
-    public function doDS($order_info, $chat_id, $message, $message_id, $parse_mode='HTML'){
+    public function doDS($order_info, $chat_id, $message, $message_id, $parse_mode='Markdown'){
 
         if ($order_info && $order_info['status'] === 1) {
             $info = $order_info['info'];
@@ -798,10 +798,10 @@ class ApiController extends Controller
             //     // log_place_order($this->code . '_get_order', "sql", M('TelegramApiOrder')->getLastSql());    //日志
             // }else{
             $message .= "\r\n最新订单详情\r\n---------------------------------------------\r\n";
-            $message .= "系统订单号：" . $info['pay_orderid'] . "\r\n";
-            $message .= "外部订单号：" . $info['out_trade_id'] . "\r\n";
+            $message .= "系统订单号：`" . $info['pay_orderid'] . "`\r\n";
+            $message .= "外部订单号：`" . $info['out_trade_id'] . "`\r\n";
             if ($info['billno'] != '') {
-                $message .= "E单号：" . $info['billno'] . "\r\n";
+                $message .= "E单号：`" . $info['billno'] . "\r\n";
             }
             $message .= "订单金额：" . $info['pay_amount'] . "\r\n";
             if ($info['pay_applydate'] != '') {
