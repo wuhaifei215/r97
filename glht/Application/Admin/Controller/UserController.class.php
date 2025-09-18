42b0246d88975364feb154b7dc4db97377e9f3c2
wuhaifei215 <wuhaifei215@gmail.com>
2025-09-18 12:00:58 +0800

    Update UserController.class.php

--- glht/Application/Admin/Controller/UserController.class.php
+++ glht/Application/Admin/Controller/UserController.class.php
@@ -1284,7 +1284,6 @@ class UserController extends BaseController
                 $products[$key]['sxffixed'] = $_tmpData[$item['id']]['sxffixed'] ? $_tmpData[$item['id']]['sxffixed'] : '0.0000';
             }
         }
-        var_dump($products);
         $this->assign('userid', $userid);
         $this->assign('products', $products);
         $this->display();
