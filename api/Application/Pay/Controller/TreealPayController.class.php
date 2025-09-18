bc99f327ba754ceb39ae00fa72b1a6b2b1821dbb
wuhaifei215 <wuhaifei215@gmail.com>
2025-09-19 00:31:16 +0800

    Update TreealPayController.class.php

--- api/Application/Pay/Controller/TreealPayController.class.php
+++ api/Application/Pay/Controller/TreealPayController.class.php
@@ -72,8 +72,8 @@ class TreealPayController extends PayController
             $OrderModel = D('Order');
             $date = date('Ymd',strtotime(substr($return['orderid'], 0, 8)));  //获取订单日期
             $tablename = $OrderModel->getRealTableName($date);
-            var_dump($ans['txid']);die;
             $re_save = $OrderModel->table($tablename)->where(['pay_orderid' => $return['orderid']])->save(['three_orderid' => $ans['txid']]);
+
             log_place_order($this->code, $return['orderid'] . "----sql", $OrderModel->getLastSql());    //日志
 
             $return = ['status' => 2, 'msg' => '代付成功'];
